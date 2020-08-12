cb406b18ac413433c64d09651ca0d2e83bb9b58c
Ashley Holanda <aholanda12@gmail.com>
2020-08-12 14:02:48 -0400

    created the inquirer questions and functions for building team

--- app.js
+++ app.js
@@ -17,6 +17,131 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+function createTeam () {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "type",
+            message: "Which type of team member would you like to add?",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+                "I am done building my team"
+            ]
+
+        }
+    ]).then(function(answer) {
+        if(answer.type === "Manager") {
+            createManager();
+        }
+        if(answer.type === "Engineer") {
+            createEngineer();
+        }
+        else if (answer.type === "Intern") {
+            createIntern();
+        }
+        else {
+            render(teamMembers);
+        }
+
+    })
+}
+    
+    function createManager () {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your manager's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your manager's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your manager's email?"
+            },
+            {
+                type: "input",
+                name: "office",
+                message: "What is your manager's office number?"
+            }
+    
+        ]).then(function (answers) {
+            const manager = new Manager(answers.name, parseInt(answers.id), answers.email, parseInt(answers.office));
+            teamMembers.push(manager);
+            createTeam();
+        });
+    }
+    
+    function createEngineer () {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your engineer's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your engineer's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your engineer's email?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your engineer's github?"
+            }
+    
+        ]).then(function (answers) {
+            const engineer = new Engineer(answers.name, parseInt(answers.id), answers.email, answers.github);
+            teamMembers.push(engineer);
+            createTeam();
+        });
+    
+    }
+    
+    function createIntern () {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your intern's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your intern's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your intern's email?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is your intern's school?"
+            }
+    
+        ]).then(function (answers) {
+            const intern = new Intern(answers.name, parseInt(answers.id), answers.email, answers.school);
+            teamMembers.push(intern);
+            createTeam();
+        });
+    
+    }
+    
+    createTeam();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
